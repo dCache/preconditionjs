7bd9ad5d8abb8760571392a2eb2c9676abbe46b2
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2017-03-02 19:23:05 +0100

    add traditional class and element wrapper

---
+++ precondition-polymer-v1.js
@@ -0,0 +1,49 @@
+/**
+ * Traditional class
+ */
+let precondition = function (){};
+
+precondition.checkArgument = function()
+{
+    const expression = arguments[0] === null ? false : arguments[0];
+    let msg = this._prepareMessage(arguments);
+    if (!expression) {
+      throw new Error(msg);
+    }
+}
+
+precondition._prepareMessage = function()
+{
+    var argLength = arguments[0].length;
+    var arg = arguments[0];
+    if (argLength === 0) {
+      return "Invalid usage, please supply the condition you want to check";
+    }
+    
+    if (typeof(arg[0]) !== 'boolean') {
+        return "The argument expression must be Boolean";
+    }
+    
+    if (typeof(arg[1]) !== 'string') {
+      return "The message expression must be String";
+    }
+    
+    if (argLength == 2 && arg[1].includes("%s")) {
+      return "The error message contain indicator with no parameter to match.";
+    }
+    
+    if (arg[1].split('%s').length != argLength - 1) {
+      return "The number of indicators are not equal to " +
+        "the number of parameters.";
+    }
+    
+    var msg = arg[1];
+    var i;
+    if (argLength > 2) {
+      const len = arg[1].split('%s').length + 1;
+      for (i=2; i<len; i++) {
+        msg = msg.replace("%s", arg[i]);
+      }
+    }
+    return msg;
+}
